efb4cffdc1d94dfba1620c056f702ee16e97e712
Ashish kumar doke <ashish.doke@uconn.edu>
2017-08-31 18:48:11 -0400

    Add files via upload
    multi layer perceptron was used

---
+++ mlp+reuters+topic+classification.py
@@ -0,0 +1,98 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from __future__ import print_function
+
+import numpy as np
+import keras
+from keras.datasets import reuters
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.preprocessing.text import Tokenizer
+
+
+# In[11]:
+
+
+max_words = 1000
+batch_size = 32
+epochs = 10
+
+
+# In[3]:
+
+
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
+                                                         test_split=0.2)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+
+num_classes = np.max(y_train) + 1
+print(num_classes, 'classes')
+
+
+# In[4]:
+
+
+print('Vectorizing sequence data...')
+tokenizer = Tokenizer(num_words=max_words)
+x_train = tokenizer.sequences_to_matrix(x_train, mode='binary')
+x_test = tokenizer.sequences_to_matrix(x_test, mode='binary')
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+
+
+# In[5]:
+
+
+print('Convert class vector to binary class matrix '
+      '(for use with categorical_crossentropy)')
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+print('y_train shape:', y_train.shape)
+print('y_test shape:', y_test.shape)
+
+
+# In[9]:
+
+
+print('Building model...')
+model = Sequential()
+model.add(Dense(128, input_shape=(max_words,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.4))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+
+# In[12]:
+
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+metrics=['accuracy'])
+
+
+# In[13]:
+
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_split=0.1)
+score = model.evaluate(x_test, y_test,
+                       batch_size=batch_size, verbose=1)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+
+# In[ ]:
+
+
+
+
